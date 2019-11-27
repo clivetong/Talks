c49a97c04af93bd62f9dc55e931eb98c49a49b94
Clive Tong <email@example.com>
2019-11-27 19:55:49 +0000

    Add files

---
+++ Excel/src/App.fs
@@ -0,0 +1,59 @@
+module App
+
+open Elmish
+open Elmish.React
+open Fable.React
+open Fable.React.Props
+
+// 0 Define the state
+
+type Position = char * int
+
+type State = {
+Cols : char list
+Rows : int list 
+Active : Position option
+Cells: Map<Position, string>
+
+}
+
+// 1 Initial state
+
+let initial = 
+ {
+  Rows = [1..15]
+  Cols = ['A'.. 'K']
+  Active = None
+  Cells = Map.empty
+ }
+
+
+
+
+
+type Msg = 
+  Foo
+
+
+
+// UPDATE
+
+let update (msg:Msg) (state:State) =
+  state
+
+
+// VIEW (rendered with React)
+
+let view (state:State) dispatch =
+
+  div []
+      [ str "kakaa" ]
+
+
+
+
+// App
+Program.mkSimple (fun () -> initial) update view
+|> Program.withReactSynchronous "elmish-app"
+|> Program.withConsoleTrace
+|> Program.run
