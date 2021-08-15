0e309372cfe9689a205edab3cd9d11e3824a4627
Clive Tong <clive.tong@red-gate.com>
2021-08-15 12:00:31 +0100

    Add an example to show this isn;t on stack replacement

---
+++ EnCForTheWin/NotOSR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheInnerLoop
+{
+    static class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() => Recurse(10));
+            Console.ReadLine();
+        }
+
+        private static void Recurse(int v)
+        {
+            if (v != 0)
+            {
+                Console.WriteLine("Entering2");
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                Recurse(v - 1);
+                Console.WriteLine("Exiting2");
+            }
+        }
+    }
+}
