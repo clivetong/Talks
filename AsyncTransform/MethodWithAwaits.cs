6cde9b3967826d22e0cd2c6081ca015d614a3ced
Clive Tong <clive.tong@red-gate.com>
2023-02-22 11:30:39 +0000

    Start an example of how async/await is compiled

---
+++ AsyncTransform/MethodWithAwaits.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace AsyncTransform;
+
+[TestFixture]
+public class MethodWithAwaits
+{
+    [TestCase(11, ExpectedResult = 12)]
+    public async Task<int> TransformThis(int argument)
+    {
+        var i = argument;
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        i++;
+
+        Console.WriteLine(i);
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        return i;
+    }
+}
