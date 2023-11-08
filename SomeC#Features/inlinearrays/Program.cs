a67bd3517a60d714717ddf3db0580caee4605b83
Clive Tong <clive.tong@red-gate.com>
2023-11-08 10:20:45 +0000

    Start working on someof the new C# features

---
+++ SomeC#Features/inlinearrays/Program.cs
@@ -0,0 +1,15 @@
+﻿Foo data = default;
+
+ref var foo0 = ref data[0];
+ref var foo1 = ref data[1];
+ref var foo2 = ref data[2];
+
+foo0 = 29;
+foo2 = 393;
+
+
+[System.Runtime.CompilerServices.InlineArray(10000)]
+struct Foo
+{
+    public int A;
+}
