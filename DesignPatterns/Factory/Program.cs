f530d235454ce6ffd71446632a80deb445fd5150
Clive Tong <clive.tong@red-gate.com>
2025-02-10 16:58:48 +0000

    First example

--- DesignPatterns/Factory/Program.cs
+++ DesignPatterns/Factory/Program.cs
@@ -1,2 +1,29 @@
 ﻿// https://refactoring.guru/design-patterns/factory-method
 
+abstract class Logistics
+{
+    public abstract ITransport CreateLogistics();
+}
+
+interface ITransport
+{
+
+}
+
+class ShipLogistics : Logistics
+{
+    public override ITransport CreateLogistics() => new Ship();
+}
+
+class Ship : ITransport
+{
+}
+
+class VehicleLogistics : Logistics
+{
+    public override ITransport CreateLogistics() => new Vehicle();
+}
+
+class Vehicle : ITransport
+{
+}
