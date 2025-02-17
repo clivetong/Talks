33502c0dc06de70c06911c490d5b71c0babf3063
Clive Tong <clive.tong@red-gate.com>
2025-02-17 17:06:28 +0000

    Add example

---
+++ DesignPatterns/State/Program.cs
@@ -0,0 +1,47 @@
+﻿// https://refactoring.guru/design-patterns/state
+
+// It appears as if the object changes its class
+
+// Related to a FSM. The outer object delegates through to the current context state
+
+// See applicability cases on the link above
+
+class Gun : IAction
+{
+    private State _context = new Unloaded();
+
+    public void PullTrigger()
+    {
+        _context.PullTrigger();
+    }
+
+    public void LoadGun()
+    {
+        _context = new Loaded();
+    }
+}
+interface IAction
+{
+    void PullTrigger();
+}
+
+abstract class State
+{
+    public abstract void PullTrigger();
+}
+
+class Unloaded : State
+{
+    public override void PullTrigger()
+    {
+        Console.WriteLine("Click!!");
+    }
+}
+
+class Loaded : State
+{
+    public override void PullTrigger()
+    {
+        Console.WriteLine("Bang!!");
+    }
+}
