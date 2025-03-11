2d91490fe20f873e660af0ce85ea5414dac0ec09
Clive Tong <clive.tong@red-gate.com>
2025-03-11 13:38:15 +0000

    Some notes on mediator

---
+++ DesignPatterns/Mediator/Program.cs
@@ -0,0 +1,8 @@
+﻿// Structure the dependencies between what would otherwise be tightly coupled classes
+
+// Not used much at Redgate - https://github.com/search?q=org%3Ared-gate%20mediator&type=code
+
+// https://refactoring.guru/design-patterns/mediator
+//  The control tower analogy
+
+
