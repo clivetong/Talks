50b09787b43c3e76a387cfe8818a32367ac6a0f6
Clive Tong <clive.tong@red-gate.com>
2025-03-11 13:51:38 +0000

    Add interpreter example

---
+++ DesignPatterns/Interpreter/Program.cs
@@ -0,0 +1,23 @@
+﻿// Define an abstract syntax for a language that is more domain specific
+
+// https://en.wikipedia.org/wiki/Interpreter_pattern
+
+// Maybe applies to an embedded SQL query
+
+var query = 
+    new OrQuery(
+    new AndQuery(new BaseTerm("Linkin Park"), new BaseTerm("Emily Armstrong")),
+    new BaseTerm("Scream Silent"));
+
+interface DoSearch
+{
+    string[] Query(QueryExpression query);
+}
+
+abstract class QueryExpression;
+class AndQuery(QueryExpression left, QueryExpression right) : QueryExpression;
+
+class OrQuery(QueryExpression left, QueryExpression right) : QueryExpression;
+
+class BaseTerm(string term) : QueryExpression;
+
