d77310e70e7f1af27040d3210e246ae146cfb723
Clive Tong <clive.tong@red-gate.com>
2025-03-24 11:39:23 +0000

    Some decorator code

--- DesignPatterns/Decorator/Program.cs
+++ DesignPatterns/Decorator/Program.cs
@@ -2,4 +2,51 @@
 
 // https://refactoring.guru/design-patterns/decorator
 
+// Allows the addition of abilities dynamically at runtime 
+
 // See Elegant Objects: https://github.com/clivetong/Talks/tree/main/ElegantObjects
+
+// Good for doing things before or after
+//     Widget
+//     WidgetWithTitle
+
+EnterBar(new Cowboy());
+EnterBar(new NonAlcoholicCowboy(new Cowboy()));
+void EnterBar(ICowboy cowboy)
+{
+    cowboy.Drink();
+    cowboy.Draw();
+}
+
+interface ICowboy
+{
+    void Drink();
+    void Draw();
+}
+
+class Cowboy : ICowboy
+{
+    public void Drink()
+    {
+        Console.WriteLine("Takes a swig of whiskey");
+    }
+
+    public void Draw()
+    {
+        Console.WriteLine("Takes out gun");
+    }
+}
+
+class NonAlcoholicCowboy(ICowboy cowboy) : ICowboy
+{
+    public void Drink()
+    {
+        Console.WriteLine("Takes a swig of water");
+        //cowboy.Drink();
+    }
+
+    public void Draw()
+    {
+        cowboy.Draw();
+    }
+}
