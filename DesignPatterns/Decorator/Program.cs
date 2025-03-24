44693e5221e5444c0a1dbaf519d6826dffd12f82
Clive Tong <clive.tong@red-gate.com>
2025-03-24 11:26:13 +0000

    Two more patterns

---
+++ DesignPatterns/Decorator/Program.cs
@@ -0,0 +1,5 @@
+﻿// Add extra functionality to an object
+
+// https://refactoring.guru/design-patterns/decorator
+
+// See Elegant Objects: https://github.com/clivetong/Talks/tree/main/ElegantObjects
---
+++ DesignPatterns/Memento/Program.cs
@@ -0,0 +1,5 @@
+﻿// https://refactoring.guru/design-patterns/memento
+
+// Serialize state but from the inside
+//  - more than just JSON serializing the public fields of an object
+
