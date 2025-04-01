3c966b34e175268a90aec612af1235e0aa6dc217
Clive Tong <clive.tong@red-gate.com>
2025-04-01 17:28:18 +0100

    Work on this week's patterns

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,1 @@
+﻿// https://refactoring.guru/design-patterns/abstract-factory
---
+++ DesignPatterns/Bridge/Program.cs
@@ -0,0 +1,4 @@
+﻿// https://refactoring.guru/design-patterns/bridge
+
+// PIMPL in C++
+//    https://en.wikipedia.org/wiki/Bridge_pattern#:~:text=The%20bridge%20pattern%20can%20also,in%20the%20C%2B%2B%20world.
