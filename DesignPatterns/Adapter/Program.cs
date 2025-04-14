3e8acf7a9d981247db2812402633cce94ad05be5
Clive Tong <clive.tong@red-gate.com>
2025-04-14 15:30:13 +0100

    Add some text about this week's patterns

---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,5 @@
+﻿
+// https://refactoring.guru/design-patterns/adapter
+
+// Get incompatible objects to work together
+//   - put a square peg in a round hole using a square to round adapter
---
+++ DesignPatterns/Strategy/Program.cs
@@ -0,0 +1,48 @@
+﻿
+// https://refactoring.guru/design-patterns/strategy
+
+// Feels a lot like state, which can be regarded as an extension of Strategy
+//   - we talked there about dynamic switching of the implementation at runtime (with composition over inheritance)
+
+var context = new Context();
+
+var result1 = context.Sorted([2, 1, 3, 4, 5, 4]);
+
+context.SetStrategy(new InplaceSorter());
+
+var result2 = context.Sorted([2, 1, 3, 4, 5, 4]);
+
+Console.ReadLine();
+
+interface ISorter
+{
+    int[] Sort(int[] items);
+}
+
+class Context
+{
+    private ISorter sorter = new DefaultSorter();
+
+    public void SetStrategy(ISorter sorter)
+    {
+        this.sorter = sorter;
+    }
+
+    internal class DefaultSorter : ISorter
+    {
+        public int[] Sort(int[] items) => items.Order().ToArray();
+    }
+
+    public int[] Sorted(int[] items) => sorter.Sort(items);
+}
+
+class InplaceSorter : ISorter
+{
+    public int[] Sort(int[] items)
+    {
+        var result = new List<int>(items);
+        result.Sort();
+        return result.ToArray();
+    }
+}
+
