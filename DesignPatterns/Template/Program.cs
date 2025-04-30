00640cdf05d9f3d0a8eeda0dea299614d797f8d5
Clive Tong <clive.tong@red-gate.com>
2025-04-30 20:23:16 +0100

    Start on Template and Visitor

---
+++ DesignPatterns/Template/Program.cs
@@ -0,0 +1,51 @@
+﻿// https://refactoring.guru/design-patterns/template-method
+
+// You've got some steps in a some algorithm
+// Let subclasses implement those steps as they need to
+
+var humanWalker = new HumanWalking();
+humanWalker.Motion();
+
+public abstract class StartWalking
+{
+    public void Motion()
+    {
+        Prepare();
+        StartRhythmForLeftLeg();
+        StartRhythmForRightLeg();
+        StepLeftLeg();
+        StepRightLeg();
+    }
+
+    protected virtual void Prepare() {}
+    protected abstract void StartRhythmForLeftLeg();
+
+    protected abstract void StartRhythmForRightLeg();
+
+    protected virtual void StepLeftLeg() 
+    { 
+        // Step left
+    }
+
+    protected virtual void StepRightLeg()
+    {
+        // Step right
+    }
+
+}
+
+public class HumanWalking : StartWalking
+{
+    protected override void Prepare()
+    {
+        // Lean forwards
+    }
+
+    protected override void StartRhythmForLeftLeg()
+    {
+    }
+
+    protected override void StartRhythmForRightLeg()
+    {
+    }
+}
---
+++ DesignPatterns/Visitor/Program.cs
@@ -0,0 +1,70 @@
+﻿// https://refactoring.guru/design-patterns/visitor
+
+// Separates the object representatino and the algorithms that work on them
+
+// Don't need to keep changign the classes for each new piece of functionality
+//  Export XML
+//  Export to Pdf
+
+// Double dispatching - use virtual methods to discriminate to find the right method, instead of picking the overload statically
+
+var things = new List<Thing>();
+things.Add(new BigThing());
+things.Add(new SmallThing());
+
+var processor = new Processor();
+
+foreach(var thing in things)
+{
+    if (thing is BigThing x)
+    {
+        processor.Process(x);
+    }
+    else if (thing is SmallThing y)
+    {
+        processor.Process(y);
+    }
+    else
+    {
+        throw new Exception("Failed to match");
+    }
+}
+
+//foreach (var thing in things)
+//{
+//    thing.Accept(processor);
+//}
+
+abstract class Thing
+{
+    //public abstract void Accept(Processor processor);
+}
+
+class BigThing : Thing
+{
+    //public override void Accept(Processor process)
+    //{
+    //    process.Process(this);
+    //}
+}
+
+class SmallThing : Thing
+{
+    //public override void Accept(Processor process)
+    //{
+    //    process.Process(this);
+    //}
+}
+
+
+class Processor
+{
+    public void Process(BigThing x) {}
+    public void Process(SmallThing x) { }
+}
+
+
+
+// See the Expression Problem: https://en.wikipedia.org/wiki/Expression_problem
+// and The Expression Problem and the Visitor Pattern: https://prog2.de/book/sec-java-expr-problem.html
+
