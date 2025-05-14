e7b68cf18c6f0b4644b40fbbd3c1de4b1975c12a
Clive Tong <clive.tong@red-gate.com>
2025-05-14 10:56:14 +0100

    Start writing examples of the next patterns

---
+++ DesignPatterns/ChainOfResponsibility/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://refactoring.guru/design-patterns/chain-of-responsibility
+
+// A chain of processors which can decide to handle the request or pass to the next one in the chain
+
+// https://code-maze.com/csharp-chain-of-responsibility-design-pattern
+//ASP.NET Core middleware is a prime example of the chain of responsibility pattern.
+//The middleware decouples a request's sender from its receivers by allowing multiple
+//handlers to handle it independently. Each handler addresses a specific aspect of the whole task
+
+// Localising parts of the processing into methods on their own objects
+//    (we've seen this before where we try to get rid of conditionals)
+
+// The handler knows about the next one (ie ordering is important), 
+// does some processing and can decide not to pass to the next
+
+// This has examples in bubbling and tunnelling events in browsers and WPF, where the control
+// happens outside these objects
+
+// The linked handlers are dynamic - they don't have a fixed structure
+// Order is important, but can be chosen the start of the run.
---
+++ DesignPatterns/Proxy/Program.cs
@@ -0,0 +1,54 @@
+﻿// https://refactoring.guru/design-patterns/proxy
+
+// Substitute for another object, perhaps controlling access (my favourite use),
+// or doing something before or after calling the original (perhaps even transferring the call
+// across the network)
+
+// Lazy initialization
+// Access control [Capability based protection mechanisms]
+// Remoting
+// Logging
+// Caching 
+// Reference counting (smart proxy in front of a heavyweight object)
+
+var realHeatingController = new MyHeatingController();
+var heatingController = new HeatingControllerProxy(realHeatingController);
+
+heatingController.TurnOn();
+
+interface IHeatingController
+{
+    void TurnOn();
+    void TurnOff();
+}
+
+class MyHeatingController : IHeatingController
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Heating on");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Heating off");
+    }
+}
+
+class HeatingControllerProxy(IHeatingController proxy) : IHeatingController
+{
+    public void TurnOn()
+    {
+        var user = Environment.GetEnvironmentVariable("USERNAME");
+        if (user == "clive.tong")
+        {
+            proxy.TurnOn();
+        }
+        else
+        {
+            throw new Exception("Only clive can turn the heating on!!!");
+        }
+    }
+
+    public void TurnOff() => proxy.TurnOff();
+}
