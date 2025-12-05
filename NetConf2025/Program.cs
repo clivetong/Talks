c9e67d1922823f2a064c3fbc25d381adc7bc8154
Clive Tong <clive.tong@red-gate.com>
2025-12-05 19:43:12 +0000

    Add test framework

---
+++ NetConf2025/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+const int Iters = 100_000;
+
+Stopwatch sw = Stopwatch.StartNew();
+
+while (true)
+{
+    long mem = GC.GetAllocatedBytesForCurrentThread();
+    sw.Restart();
+
+    for(int i =0; i < Iters; i++)
+    {
+        Test();
+    }
+
+    sw.Stop();
+    mem = GC.GetAllocatedBytesForCurrentThread() - mem;
+    Console.WriteLine($"{sw.Elapsed.TotalSeconds * 1_000_000_000 / Iters:N0} ns, {mem/Iters:N0} bytes" );
+}
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void Test()
+{
+    
+}
